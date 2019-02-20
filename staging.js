cf1819eb7cbb72c1572c7eedb59daffc157d5738
Mark Murphy <mark.murphy@asos.com>
2019-02-20 11:35:15 +0000

    Added 'debug' code and dependency

--- staging.js
+++ staging.js
@@ -1,45 +1,72 @@
+var debug = require('debug')('staging');
 var dns = require('dns');
 
+debug('staging.js Entry: %O', process.argv);
 // Loop through command line parameters.  Expecting 'staging.js fqdn [fqdn [fqdn] ... ]'
 for (i = 2; i < process.argv.length; i++) {
+  debug('Extracted "%s" from the command line', process.argv[i]);
   // Get the Staging IP address of each fully qualified domain name specified and print to the console
   getStagingIPAddress(process.argv[i]);
 };
 
+debug('staging.js Exit');
+
 function getStagingIPAddress(hostname){
+  debug('getStagingIPAddress() Entry');
   // Resolve the hostname to its aliases
+  debug('Calling dns.resolveCname(%s)', hostname);
   dns.resolveCname(hostname, function (err, aliases) {
-    // Pick the 1st alias
-    var alias = aliases[0];
-    
-    // Break down the alias hostname into its constituent parts/segments
-    var segments = alias.split(".");
-    
-    // append "-staging" to the penultimate segment
-    var stagingDomain = segments[segments.length - 2] + "-staging";
-    
-    // Replace the penultimate segment with the "-staging" varient
-    segments[segments.length - 2] = stagingDomain;
+    if (err) {
+      debug('An error occurred: %O', err);
+    } else {
+      debug('Processing returned aliases: %O', aliases);
 
-    // Variable where we're going to store the fully qualified Staging varient
-    var stagingFQDN = "";
+      // Pick the 1st alias
+      debug('Using the 1st alias "%s"', aliases[0])
+      var alias = aliases[0];
 
-    segments.forEach(element => {
-    // Loop through the segments rebuilding them back into a fully qualified domain name
-      stagingFQDN = stagingFQDN + element + ".";
-    });
+      // Break down the alias hostname into its constituent parts/segments
+      var segments = alias.split(".");
+      debug('Split into segments: %O', segments);
 
-    // Resolve the Staging varient fqdn to an IP address
-    var stagingIPAddress = dns.resolve(stagingFQDN, function (err, addresses) {
-      //if (err) throw err;
-      // Return the 1st IP address
-      console.log(stagingIPAddress);
-      return addresses[0];
-    });
-    
-    // Return the Staging IP address along with the orginal hostname formatted as a hosts file entry
-    //return stagingIPAddress + " " + hostname
+      // append "-staging" to the penultimate segment
+      var stagingDomain = segments[segments.length - 2] + "-staging";
+      debug('Going to be using "%s" instead of "%s"', stagingDomain, segments[segments.length - 2]);
 
-    console.log(stagingIPAddress + " " + hostname);
+      // Replace the penultimate segment with the "-staging" variant
+      segments[segments.length - 2] = stagingDomain;
+      debug("Edited segments: %O", segments);
+
+      // Variable where we're going to store the fully qualified Staging variant
+      var stagingFQDN = "";
+
+      segments.forEach(element => {
+      // Loop through the segments rebuilding them back into a fully qualified domain name
+        stagingFQDN = stagingFQDN + element + ".";
+      });
+
+      debug('The Staging alias for %s is %s', hostname, stagingFQDN);
+
+      debug('Calling dns.resolve(%s)', stagingFQDN);
+      // Resolve the Staging variant fqdn to an IP address
+      var stagingIPAddress = dns.resolve(stagingFQDN, function (err, addresses) {
+        // *** to do *** if (err) throw err;
+
+        debug('DNS returned: %O', addresses);
+        debug('Returning: %s', addresses[0]);
+        // Return the 1st IP address
+        console.log(stagingIPAddress);
+        return addresses[0];
+      });
+
+      // Return the Staging IP address along with the original hostname formatted as a hosts file entry
+      //return stagingIPAddress + " " + hostname
+
+      console.log(stagingIPAddress + " " + hostname);
+    }
+
+    debug('getStagingIPAddress() Exit');
   });
-}  
+
+  debug('getStagingIPAddress() Exit');
+}
