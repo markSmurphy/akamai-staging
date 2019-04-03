d4350350998eeafea667da9c2672d1cdf07fabcd
Mark Murphy <mark.murphy@asos.com>
2019-04-03 13:49:03 +0100

    Added help screen (--help)

--- akamai-staging.js
+++ akamai-staging.js
@@ -1,17 +1,44 @@
 #!/usr/bin/env node
 var debug = require('debug')('staging');
 var dns = require('dns');
-
-// Print a platform agnostic newline character first. This particularly helps when output is appended to a text file
-console.log (require('os').EOL);
+// Platform independent end-of-line character
+var endOfLine = require('os').EOL;
 
 debug('staging.js Entry: %O', process.argv);
-// Loop through command line parameters.  Expecting 'staging.js fqdn [fqdn [fqdn] ... ]'
-for (i = 2; i < process.argv.length; i++) {
-  debug('Extracted "%s" from the command line', process.argv[i]);
-  // Get the Staging IP address of each fully qualified domain name specified and print to the console
-  getStagingIPAddress(process.argv[i]) + " " + process.argv[i];
-};
+
+// Check for 'help' command line parameters, or no parameters at all
+if ((process.argv.length == 2) || (process.argv[2].toLowerCase() == "-h") || (process.argv[2].toLowerCase() == "--help")) {
+  const colours = require('colors');
+  const package = require('./package.json');
+
+  //display help screen
+  console.log('\u2726 [akamai-staging]'.cyan);
+  console.log('Read the docs: '.green + 'https://github.com/MarkSMurphy/akamai-staging#readme');
+  console.log('Support & bugs: '.magenta + 'https://github.com/MarkSMurphy/akamai-staging/issues');
+  console.log(endOfLine);
+  console.log('Returns an Akamai Staging network IP address for one or more domains.'.italic);
+  console.log(endOfLine);
+  console.log('VERSION'.grey);
+  console.log('   ' + package.version.bold);
+  console.log(endOfLine);
+  console.log('SYNTAX'.grey);
+  console.log('   ' + 'staging domain [domain [domain] ...]'.bold);
+  console.log(endOfLine);
+  console.log('EXAMPLE'.grey);
+  console.log('   staging www.akamai.com control.akamai.com');
+
+} else {
+
+  // Print a platform agnostic newline character first. This particularly helps when output is appended to a text file
+  console.log(endOfLine);
+
+  // Loop through command line parameters.  Expecting 'staging.js fqdn [fqdn [fqdn] ... ]'
+  for (i = 2; i < process.argv.length; i++) {
+    debug('Extracted "%s" from the command line', process.argv[i]);
+    // Get the Staging IP address of each fully qualified domain name specified and print to the console
+    getStagingIPAddress(process.argv[i]) + " " + process.argv[i];
+  };
+}
 
 function getStagingIPAddress(hostname){
   debug('getStagingIPAddress() Entry');
